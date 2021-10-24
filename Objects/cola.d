01937e231da87748cc4391a62cabbef92ea8ce08
AcquaDiGiorgio <jorgelisa.2000@gmail.com>
2021-10-24 13:22:26 +0200

    hmn

--- Objects/cola.d
+++ Objects/cola.d
@@ -1,6 +1,5 @@
 .\objects\cola.o: cola.c
-.\objects\cola.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
-.\objects\cola.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\cola.o: C:\Keil_v5\ARM\INC\Philips\LPC210X.H
 .\objects\cola.o: cola.h
-.\objects\cola.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\inttypes.h
-.\objects\cola.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\cola.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\cola.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
--- cola.h
+++ cola.h
@@ -1,7 +1,7 @@
-void inicializacion(Lista *l);
-int cantidad(Lista *l);
-void insertarPrimero(Lista *l,Info *x);
-void insertarUltimo(Lista *l, Info *x);
-void liberar(Lista *l);
-struct Informacion borrarElemento(Lista *l);
-void imprimir(Lista *l);	
+//void inicializacion(Lista *l);
+//int cantidad(Lista *l);
+//void insertarPrimero(Lista *l,Info *x);
+//void insertarUltimo(Lista *l, Info *x);
+//void liberar(Lista *l);
+//struct Informacion borrarElemento(Lista *l);
+//void imprimir(Lista *l);	
