66a366e3422f2e28759dc0737bd8323cdce4c352
AcquaDiGiorgio <jorgelisa.2000@gmail.com>
2021-10-25 13:14:18 +0200

    Malloc Runtime Error

--- cola.h
+++ cola.h
@@ -1,7 +1,34 @@
-//void inicializacion(Lista *l);
-//int cantidad(Lista *l);
-//void insertarPrimero(Lista *l,Info *x);
-//void insertarUltimo(Lista *l, Info *x);
-//void liberar(Lista *l);
-//struct Informacion borrarElemento(Lista *l);
-//void imprimir(Lista *l);	
+#ifndef COLA_H
+#define COLA_H
+
+#include <inttypes.h>
+
+//Struct que almacena la informacion del evento 
+typedef struct Informacion {
+    int ID_evento;
+    int auxData;
+    int marca_temporal;
+}Info;
+
+//Struct que representa un nodo de la lista circular
+struct nodo {
+    struct Informacion dato;
+    struct nodo *sig;
+    struct nodo *ant;
+};
+
+//Struct que representa una lista circular que almacena informacion de eventos
+typedef struct Lista {
+     struct nodo *ini;
+     int tamano;
+}Lista;
+
+Info newInfo(int ID, int auxData, int timeStamp);
+void inicializarLista(Lista *l, Info x);
+int size(Lista *l);
+//void insertarPrimero(Lista *l,Info x);
+void insertar(Lista *l, Info x);
+void liberar(Lista *l);
+struct Informacion sacarElemento(Lista *l);
+
+#endif //EVENTOS_H
